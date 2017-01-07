b4b3dec76e91a57562fb13450e4bbbc06315f946
MDG <limbar@hotmail.com>
2017-01-07 11:55:14 -0800

    Changes to the calculator. Added floating point capability. Refined messages, exitting and -help messages. Refined error handling.

--- MySolutions/Calc/calc_source.cpp
+++ MySolutions/Calc/calc_source.cpp
@@ -1,39 +1,81 @@
 
 #include <iostream>
-#include <string>
 #include <cstdlib>
+#include <string>
 
 #define NOTIF_ERROR "\nInvalid notation!\nPlease, utilize Polish Notation for arguments (-help for correct usage).\n"
 
-int isInt(char* op)
+/**
+*
+* @author Matheus Depra Gudergues
+*
+* @brief This program emulates a simple calculator (Assignment 1).
+*
+* @return 0 if everything executed correctly
+*
+*/
+
+bool isOperator(char* op)
+{
+	if ((sizeof(op) >= 2) && (op[1] == '\0')
+		&& (op[0] == '+' || op[0] == '-' || op[0] == '*' || op[0] == '/'))
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool isInteger(char* op)
 {
 	// empty string (shouldn't be possible)
 	if (sizeof(op) == 0)
 	{
-		return 0;
+		return false;
 	}
 	for (int i = 0; op[i] != '\0'; i++)
 	{
-		if (op[i] != '\0' && (op[i] < '0' || op[i] > '9'))
+		if (op[i] < '0' || op[i] > '9')
 		{
-			// operator
-			if (op[i] == '+' || op[i] == '-' || op[i] == '*' || op[i] == '/')
-			{
-				return -1;
-			}
-			
-			// invalid string
-			return 0;
+			return false;
 		}
 	}
 
-	// is a number
-	return 1;
+	return true;
+}
+
+bool isFloatingPoint(char* op)
+{
+	bool fpoint = false;
+
+	// empty string (shouldn't be possible)
+	if (sizeof(op) == 0)
+	{
+		return false;
+	}
+	for (int i = 0; op[i] != '\0'; i++)
+	{
+		if (op[i] == '.')
+		{
+			if (fpoint)
+			{
+				return false;
+			}
+			
+			fpoint = true;
+		}
+		else if (op[i] < '0' || op[i] > '9')
+		{
+			return false;
+		}
+	}
+
+	return fpoint;
 }
 
 int operate(int carry, char* num, char* op)
 {
-	if (isInt(num) != 1 || isInt(op) != -1)
+	if (!isInteger(num) || !isOperator(op))
 	{
 		std::cout << NOTIF_ERROR << "(Error message: invalid character/order reached)\n";
 	}
@@ -59,35 +101,80 @@ int operate(int carry, char* num, char* op)
 	return carry;
 }
 
+double operate(double carry, char* num, char* op)
+{
+	if ((!isInteger(num) && !isFloatingPoint(num)) || !isOperator(op))
+	{
+		std::cout << NOTIF_ERROR << "(Error message: invalid character/order reached)\n";
+
+		getchar();
+		exit(0);
+	}
+	else
+	{
+		switch (op[0])
+		{
+		case '+':
+			carry += atof(num);
+			break;
+		case '-':
+			carry -= atof(num);
+			break;
+		case '*':
+			carry *= atof(num);
+			break;
+		case '/':
+			carry /= atof(num);
+			break;
+		}
+	}
+
+	return carry;
+}
+
 int main(int argc, char* argv[])
 {
-	int result = 0,
-		isnum = 0;
+	bool float_flag = false;
+	int iresult = 0;
+	double fresult = 0.0f;
 
 	if (argc > 1)
 	{
 		if (strcmp(argv[1], "-help") == 0)
 		{
 			std::cout << std::endl << "Usage:" << std::endl << std::endl;
-			std::cout << "\tThis is a simple calculator that does integer arithmatic "
-				"calculations by using Polish Notation." << std::endl << std::endl;
-			std::cout << "\tFor example:\n\n"
-				"\t\tCalc int1 int2 operation (int3 op int4 op ...)"
+			std::cout << "\tThis is a simple calculator that evaluates integer and floating point "
+				"arithmatic calculations by using Polish Notation for its arguments." << std::endl << std::endl;
+			std::cout << "\tFor example, execute the program as:\n\n"
+				"\t\tCalc.exe int1 int2 operation (int3 op int4 op ...)\n"
+				"\t\tCalc.exe float1 float2 operation (float3 op float4 op ...)\n"
+				"\t\tCalc.exe int1 float2 operation (float3 op int4 op ...)"
 				<< std::endl << std::endl;
 		}
 		else
 		{
-			isnum = isInt(argv[1]);
-
-			if (isnum == 1)
+			if (isInteger(argv[1]))
 			{
-				result = std::atoi(argv[1]);
+				iresult = std::atoi(argv[1]);
 
 				for (int i = 2; i < argc; i += 2)
 				{
 					if (i + 1 < argc)
 					{
-						result = operate(result, argv[i], argv[i + 1]);
+						if (float_flag)
+						{
+							fresult = operate(fresult, argv[i], argv[i + 1]);
+						}
+						else if (isFloatingPoint(argv[i]))
+						{
+							float_flag = true;
+							fresult = static_cast<double>(iresult);
+							fresult = operate(fresult, argv[i], argv[i + 1]);
+						}
+						else
+						{
+							iresult = operate(iresult, argv[i], argv[i + 1]);
+						}
 					}
 					else
 					{
@@ -97,7 +184,34 @@ int main(int argc, char* argv[])
 					}
 				}
 
-				std::cout << "Result: " << result << std::endl;
+				if (float_flag)
+				{
+					std::cout << std::showpoint << "Result: " << fresult << std::endl;
+				}
+				else
+				{
+					std::cout << "Result: " << iresult << std::endl;
+				}
+			}
+			else if (isFloatingPoint(argv[1]))
+			{
+				fresult = std::atof(argv[1]);
+
+				for (int i = 2; i < argc; i += 2)
+				{
+					if (i + 1 < argc)
+					{
+						fresult = operate(fresult, argv[i], argv[i + 1]);
+					}
+					else
+					{
+						std::cout << NOTIF_ERROR << "(Error message: invalid length of arguments)\n";
+
+						return 0;
+					}
+				}
+
+				std::cout << std::showpoint << "Result: " << fresult << std::endl;
 			}
 			else
 			{
